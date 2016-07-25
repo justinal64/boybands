b4364fdd6cd92599f1c16a0722659abbb00de667
justin leggett <justin@codingrookie.com>
2016-07-25 09:14:00 -0500

    modify boybands.js to complete the task

--- boybands.js
+++ boybands.js
@@ -11,19 +11,27 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 
   // Add the band names into the correct <div>
-  currentBand = ???;
-
+  // currentBand = ???;
+  bandElement.innerHTML += "<li>" + bands[loopTracker] + "</li>";
 
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  // currentVeggie = ???;
+  veggieElement.innerHTML += "<li>" + vegetables[loopTracker] + "</li>";
 
 }
+
+
+
+
+
+// Example
+// hohohoElement.innerHTML +=  "<h3>" + colors[i] + " " + reindeer[i] + "</h3>";
